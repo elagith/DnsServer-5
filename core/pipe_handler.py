d8505ff9c5efc8243560da895980bd8a9713a54b
Eran Elbaz <erank75@gmail.com>
2018-04-17 23:28:31 +0300

    add types

--- core/pipe_handler.py
+++ core/pipe_handler.py
@@ -1,12 +1,14 @@
 import logging.handlers
+import multiprocessing
+import logging
 
 
 class PipeHandler(logging.Handler):
-    def __init__(self, pipe):
+    def __init__(self, pipe: multiprocessing.Pipe):
         logging.Handler.__init__(self)
         self.pipe = pipe
 
-    def emit(self, record):
+    def emit(self, record: logging.LogRecord):
         try:
             self.pipe.send(record)
         except OSError:
